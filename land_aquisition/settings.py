f24eb6c7f8969cb690f6be2ba3206166ab2ded16
Ihsanullah Siddique <ihsanullahi501@gmail.com>
2024-05-27 20:03:25 +0500

    Changed loading static

--- land_aquisition/settings.py
+++ land_aquisition/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
